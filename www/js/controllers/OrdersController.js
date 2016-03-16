3ee5312aaf86b5fdcdeee470aa44b3451097b1a7
Jéssika Renally Ribeiro Rodrigues <reh.nally@gmail.com>
2016-03-16 20:20:33 -0300

    ajeitando a requisicao para pegar os orders do client logado

--- www/js/controllers/OrdersController.js
+++ www/js/controllers/OrdersController.js
@@ -3,38 +3,17 @@ var servicesModule = angular.module('service', []);
 servicesModule.controller('OrdersController', function ($scope, $http, $cordovaToast, OrderService) {
 	var self = this;
 
-	self.services = [
-		{
-			description: "Óticas Diniz - Reparo na armação", 
-			client: "Pedro",
-			status: "Confeccionando",
-			creation_date: "12/02/2016"
-		},
-		{
-			description: "Gato & Sapato - Personalização de sandália", 
-			client: "Pericles",
-			status: "Entregue",
-			creation_date: "12/02/2016"
-		},
-		{
-			description: "Gráfica Copiar - Confecção das comandas", 
-			client: "Luana",
-			status: "Pronto",
-			creation_date: "12/02/2016"			
-		},
-		{
-			description: "Laboratórios Bem Estar - Exame de sangue", 
-			client: "Leticia",
-			status: "Confeccionando",
-			creation_date: "12/02/2016"
-		}
-	];
+	self.services;
 
 	(function main(){
-		var promise = OrderService.getOrder("1gz2a4b");
+		var promise = OrderService.getOrder("56e809e842046b891eb6a125");
 		promise.then(function (response){
 			if (response != undefined) {
-				//self.services = response.result;
+				console.log(response);
+				console.log(response.data.result);
+
+				self.services = [response.data.result];
+				console.log(self.services);
 			}
 		})
 	})();
